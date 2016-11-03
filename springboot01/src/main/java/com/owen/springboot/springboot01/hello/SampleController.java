c894e2ae02ee930844a6c7e05891dc0ed5d86f70
xuhj <359760683@qq.com>
2016-11-03 17:30:49 +0800

    Add test for SimpleController

--- springboot01/src/main/java/com/owen/springboot/springboot01/hello/SampleController.java
+++ springboot01/src/main/java/com/owen/springboot/springboot01/hello/SampleController.java
@@ -2,25 +2,24 @@ package com.owen.springboot.springboot01.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by owen on 2016/10/19.
  * @SpringBootApplication 相当于 @Configuration、@ComponentScan和@EnableAutoConfiguration
  */
-@Controller
+@RestController
 @SpringBootApplication
 public class SampleController {
     @ResponseBody
-    @RequestMapping(value="/")
+    @RequestMapping(value="/hello")
     String home(){
         return "Hello World!";
     }
 
     public static void main(String[] args){
-        //SpringApplication.run(SampleController.class, "--server.port=8081");
         SpringApplication.run(SampleController.class, args);
     }
 }
---
+++ springboot01/src/test/java/com/owen/springboot/springboot01/SampleControllerTest.java
@@ -0,0 +1,41 @@
+package com.owen.springboot.springboot01;
+
+import com.owen.springboot.springboot01.hello.SampleController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by owen on 2016/11/3.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration
+public class SampleControllerTest {
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new SampleController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+}
